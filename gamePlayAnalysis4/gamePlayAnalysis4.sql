ae6a659d6c9e371679ca5651885e7bb2ed3a7bf1
Claudiu Apetroaei <38036145+ApetroaeiClaudiu@users.noreply.github.com>
2023-09-15 19:40:47 +0300

    Create gamePlayAnalysis4.sql

---
+++ gamePlayAnalysis4/gamePlayAnalysis4.sql
@@ -0,0 +1,6 @@
+SELECT ROUND(COUNT(*) / (SELECT COUNT(DISTINCT player_id) FROM Activity),2) as fraction 
+FROM Activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN 
+(
+    SELECT player_id, MIN(event_date) as first_login FROM Activity GROUP BY player_id
+)
